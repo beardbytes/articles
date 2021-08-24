82a843c8a506a9fc25d9f278bec9518dd0e5846a
maheshwariAditya <aditya.maheshwari@abzooba.com>
2021-08-24 13:34:16 +0530

    Configuration file

--- web/config.py
+++ web/config.py
@@ -4,27 +4,13 @@ import yaml
 with open("web/config.yaml", 'r') as stream:
     data_loaded = yaml.safe_load(stream)
 
-# for k, v in data_loaded.items():
-#     if k == "API":
-#         for key, value in data_loaded[k].items():
-#             print(value[:])
-
-host = data_loaded["ELASTICSEARCH"]['host']
+# to get the host, url, index_name and port of elastic search
+elastic_host = data_loaded["ELASTICSEARCH"]['host']
 url = data_loaded["ELASTICSEARCH"]['url']
 index_name = data_loaded["ELASTICSEARCH"]['index_name']
 elastic_port = data_loaded["ELASTICSEARCH"]['port']
 
+# to get the port, debug and host of flask
 flask_port = data_loaded["API"]['port']
 debug = data_loaded["API"]['debug']
-# Get the port, host, url and index_name of elastic search
-# print(config_object)
-# host = elastic_search["host"]
-# print(host)
-# elastic_port = elastic_search["port"]
-# url = elastic_search["url"]
-# index_name = elastic_search["index_name"]
-
-# # Get the port number, port and debug of flask
-# web = config_object["API"]
-# flask_port = web["port"]
-# debug = web["debug"]
+flask_host = data_loaded["API"]['host']
